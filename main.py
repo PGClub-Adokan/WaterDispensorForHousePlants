669e9102288582be6a4c9d31e06db3a1fc200b51
SMOKYWORKS\Smoky <1y.xxxsmokyxxx.y1@gmail.com>
2025-03-01 18:32:57 +0900

    added comment

--- main.py
+++ main.py
@@ -13,15 +13,21 @@ if __name__ == "__main__":
 
     # get water sensor value
     voltage = sensor.get_sensor_value_average()
+    print("sensor value: %s" % voltage)
 
     try:
         safety_counter = 0
         # start water dispence
         while voltage < constants.WATER_SENSOR_LIMIT or safety_counter <= 10:
+            print("water dispence start")
             gpio.on()
             time.sleep(60 * 1)  # do water dispense 1 minute
+            print("water dispence rest")
             gpio.off()
             time.sleep(60 * 4)  # rest water dispence 4 minute
+            voltage = sensor.get_sensor_value_average()
+            print("sensor value: %s" % voltage)
             safety_counter += 1
     except:
+        gpio.off()
         gpio.cleanup
